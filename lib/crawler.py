38c9aae2ff478390e9296995bed619b9704fa709
zkx <herozllz@163.com>
2015-04-04 07:46:28 +0800

    update crawler

--- lib/crawler.py
+++ lib/crawler.py
@@ -1,2 +1,17 @@
-import 
+import urllib
+import httplib
+
+def fetchurl(url,method):
+	proto, rest = urllib.splittype(url)
+	domain,rest = urllib.splithost(rest)
+	conn = httplib.HTTPConnection(domain)
+	conn.request(method, rest)
+	res = conn.getresponse()
+	if res.status != 200:
+		print "fetch%serror,with status:%d" %(url,res.status)
+		data = ""
+	else:
+		data = res.read()
+		conn.close()
+	return dataimport 
 
